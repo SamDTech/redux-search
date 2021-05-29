919215c07f36f97e5d55b3a5a07d158edce8bdd3
damilare <opeyemisamuel222@gmail.com>
2021-05-29 22:59:03 +0100

    form events

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+
+  const submit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+  };
   return (
     <div>
-      <form action="">
-        <input type="text" />
+      <form onSubmit={submit}>
+        <input type="text" value={term} onChange={e=> setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
