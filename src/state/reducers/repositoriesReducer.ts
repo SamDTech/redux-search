345abd6c0f6c98fb0fcc2b1acef18cdd23e9c2e3
damilare <opeyemisamuel222@gmail.com>
2021-05-29 15:27:44 +0100

    typing an action

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,14 +4,19 @@ interface RepositoryState {
   data: string[];
 }
 
-const reducer = (state: RepositoryState, action: any): RepositoryState => {
+interface Action {
+  type: string;
+  payload: any;
+}
+
+const reducer = (state: RepositoryState, action: Action): RepositoryState => {
   switch (action.type) {
     case "search_repositories":
       return { ...state, loading: true, error: null };
     case "search_repositories_success":
       return { ...state, loading: false, data: action.payload };
     case "search_repositories_error":
-      return { ...state, loading: false,  error: action.payload };
+      return { ...state, loading: false, error: action.payload };
 
     default:
       return state;
