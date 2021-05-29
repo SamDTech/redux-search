bf5b9781da7618bc12c9bf1fcee386c813cb3770
damilare <opeyemisamuel222@gmail.com>
2021-05-29 22:25:44 +0100

    adding creation logic

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -6,9 +6,13 @@ const searchRepositories = (term: string) => async (dispatch: any) => {
   dispatch({ type: ActionType.SEARCH_REPOSITORY });
 
   try {
-    const { data } = await axios.get("/");
-
-    dispatch({ type: ActionType.SEARCH_SUCCESS_REPOSITORY, payload: data });
+    const { data } = await axios.get(`https://registry.npmjs.org/-/v1/search`, {
+        params: {
+            text: term
+        }
+    });
+    const result = data.objects.map((result: any)=> result.package.name) 
+    dispatch({ type: ActionType.SEARCH_SUCCESS_REPOSITORY, payload: result });
   } catch (error) {
     dispatch({
       type: ActionType.SEARCH_ERROR_REPOSITORY,
