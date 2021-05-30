5acaf076a3aa904cc96523e4e01b05d2aec8c0f9
damilare <opeyemisamuel222@gmail.com>
2021-05-30 17:55:48 +0100

    consume store

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -25,6 +25,12 @@ const RepositoriesList: React.FC = () => {
         />
         <button type="submit">Search</button>
       </form>
+
+      {error && <h3>{error}</h3>}
+      {loading && <h3>loading...</h3>}
+      {!error && !loading && data && data.map((name) =>(
+          <div key={name}>{name}</div>
+      ))}
     </div>
   );
 };
